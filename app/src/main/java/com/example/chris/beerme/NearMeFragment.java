63d99c2179940336595806abce04ab836d9907d9
Chris <chagmaier@oxy.edu>
2018-04-03 18:46:54 -0700

    first commit

---
+++ app/src/main/java/com/example/chris/beerme/NearMeFragment.java
@@ -0,0 +1,31 @@
+package com.example.chris.beerme;
+
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class NearMeFragment extends Fragment {
+
+
+    public NearMeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_near_me, container, false);
+    }
+
+}
